cd8e918326cac1257abb2bbac0f0f76159288b90
vskendo <smartcrazylin@gmail.com>
2022-09-10 12:07:55 +0800

    use element plus

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <AppIndex />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import AppIndex from './views/AppIndex.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    AppIndex
   }
 })
 </script>
---
+++ src/api/post.ts
@@ -0,0 +1,5 @@
+export function api() {
+  const a: { val?: number } = { val: undefined }
+  a.val = 1
+  return a
+}
